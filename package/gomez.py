736d36542a709f19cfd5548b2baf3b060a0f1cfc
gomez-ashleyhermione <hermionegomez49@gmail.com>
2024-12-11 19:37:33 +0800

    add changes in my module gomez

--- package/gomez.py
+++ package/gomez.py
@@ -5,29 +5,34 @@ def menu():
         print("Welcome! My name is Ashley Hermione Gomez.")
         print("1. Basic Info")
         print("2. Goals")
-        print("[3] Comment from De Leon")
-        print("[4] Comment from Esparagoza")
-        print("[5] Comment from Alejandro")
-        print("[6] Comment from Mosenos")
+        print("3. Comment from De Leon")
+        print("4. Comment from Esparagoza")
+        print("5. Comment from Alejandro")
+        print("6. Comment from Mosenos")
+        print("7. Exit")  # Add an option for exiting
         choice = input("Choose an option: ")
         
         if choice == "1":
             os.system('cls')
-            print("I am Diploma information technology major, and I enjoy programming and playing games.")
+            print("Nickname: Ash")
+            print("Course: Diploma in Information Technology")
         elif choice == "2":
             os.system('cls')
             print("My goal is to become a game developer and contribute to open-source projects.")
         elif choice == '3':
             os.system('cls')
-            print ("------ Comment from De Leon ------")
+            print("------ Comment from De Leon ------")
         elif choice == '4':
-                os.system('cls')
-                print ("------ Comment from Esparagoza ------")   
+            os.system('cls')
+            print("------ Comment from Esparagoza ------")   
         elif choice == '5':
             os.system('cls')
-            print ("------ Comment from Alejandro------")
+            print("------ Comment from Alejandro ------")
         elif choice == '6':
             os.system('cls')
-            print ("------ Comment from Mosenos ------") 
+            print("------ Comment from Mosenos ------")
+        elif choice == '7': 
+            print("\nExiting program.\n")
+            break
         else:
             print("Invalid choice. Please try again.")
